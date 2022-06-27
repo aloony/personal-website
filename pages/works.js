bf9d83f7b45410e09434d2a6f9a3eb0ad870aaa5
aloony <npp3rzwi@gmail.com>
2022-06-27 23:10:39 +0200

    fix imports

--- pages/works.js
+++ pages/works.js
@@ -6,7 +6,7 @@ import { WorkGridItem } from '../components/grid-item'
 import thumbB2B from '../public/images/works/b2b_eyecatch.png'
 import thumbCryptoFarm from '../public/images/works/crypto-farm_eyecatch.png'
 import thumbDiscord from '../public/images/works/discord_eyecatch.png'
-import thumbPortfolio from '../public/images/works/portfolio_eyecatch.png'
+// import thumbPortfolio from '../public/images/works/portfolio_eyecatch.png'
 import thumbWierdMerch from '../public/images/works/wierdmerch_eyecatch.jpg'
 
 const Works = () => (
